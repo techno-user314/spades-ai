39c24866fd416a4eacaa082d1d3ab8dff5de7ffb
techno-user314 <zharwood314@gmail.com>
2026-04-27 10:06:15 -0600

    Add minimal player logic

--- src/players.py
+++ src/players.py
@@ -1,9 +1,16 @@
 class Player:
     def __init__(self, hand):
         self.cards = hand
+        self.tricks = 0
+
 
     def bid(self):
-        pass
+        return 1
+
 
     def play(self, table):
-        pass
+        return self.cards.play(0)
+
+
+    def did_win(self, did_i_win):
+        self.tricks += int(did_i_win)
