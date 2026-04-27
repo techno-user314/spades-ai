9bbd2e8554f044a66fcff8562dc74f1fa90a3d92
techno-user314 <zharwood314@gmail.com>
2026-04-27 10:09:13 -0600

    Able to simulate a round of Spades
    No bidding, simply throwing cards

--- src/main.py
+++ src/main.py
@@ -8,9 +8,31 @@ PLAYER_COUNT = 4
 class Game:
     def __init__(self):
         deck = CardDeck()
+        self.rounds = 0
 
-        players = [Player(h) for h in deck.deal(PLAYER_COUNT)]
+        self.players = [Player(h) for h in deck.deal(PLAYER_COUNT)]
 
 
     def do_round(self):
-        pass
+        self.rounds += 1
+
+        on_table = []
+        winner = 0
+        for player in self.players:
+            on_table.append(player.play(on_table))
+
+        for i, card in enumerate(on_table[1:]):
+            if card > on_table[winner]:
+                winner = i
+
+        for i, player in enumerate(self.players):
+            player.did_win(winner==i)
+
+        for card in on_table:
+            print(card)
+        print(winner)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.do_round()
